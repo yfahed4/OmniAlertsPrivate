7915c63a74abe1da060796e2aed64e625d1a6348
xam <petrofahed@hotmail.com>
2023-01-27 09:41:59 +0100

    OAMac Map

---
+++ Shared/OmniAlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmniAlertsApp.swift
+//  Shared
+//
+//  Created by xam on 10/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct OmniAlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
